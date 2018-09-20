6796df5400153f66c840592d1be9ccf639fb9363
erinbevec <42977057+erinbevec@users.noreply.github.com>
2018-09-19 20:50:33 -0400

    Add files via upload

---
+++ StringThing.java
@@ -0,0 +1,22 @@
+
+public class StringThing 
+{
+	public static void main(String args[])
+	{
+
+	String river = "Mississippi";
+	int length = river.length();
+	System.out.println(length);
+	
+	String greeting = "Hello, world";
+	System.out.println(greeting.toUpperCase());
+	
+	river.println();
+	
+	System.out.println(river.replace("p", "s"));
+	
+	
+	System.out.println(river.toUpperCase());
+	}	
+}
+ 
